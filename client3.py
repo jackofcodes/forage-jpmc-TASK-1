71280cde37481791acd654887051984b21c440ef
jackofcodes <aamirlodhia35@gmail.com>
2023-11-13 20:45:44 +0530

    The required changes have been made

--- client3.py
+++ client3.py
@@ -35,18 +35,23 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return 0
+    
+    ratio = price_a / price_b
+    return ratio
 
 
 # Main
 if __name__ == "__main__":
+    # Initialize a dictionary to store stock prices
+    prices = {}
+
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
@@ -54,6 +59,18 @@ if __name__ == "__main__":
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+            # Store the stock price in the dictionary
+            prices[stock] = price
+
+            # Print the correct values
+            print(f"Quoted {stock} at (bid:{bid_price}, ask:{ask_price}, price:{price})")
+
+        # Calculate and print the ratio using the stored prices
+        stock_a = 'ABC'  # Replace with actual stock symbols
+        stock_b = 'DEF'  # Replace with actual stock symbols
+        ratio = getRatio(prices[stock_a], prices[stock_b])
+        print(f"Ratio for {stock_a} and {stock_b}: {ratio}")
+
+    # You can perform further analysis or visualization on the results here
+    # For example, calculating the average ratio, plotting results, etc.
